0d0ad14724aa77a382a900ab27c91d2b0379ce51
AverJung <usorname1@gmail.com>
2025-06-29 19:49:22 -0700

    Manage-streaks

---
+++ src/app/manage-streaks/page.tsx
@@ -0,0 +1,10 @@
+export default function manageStreaks() {
+    return(
+        <div>  
+            <h1>Ongoing Streaks:</h1>
+        
+        </div>
+        // list the users friend streaks and allow them to manage
+    )
+
+}
