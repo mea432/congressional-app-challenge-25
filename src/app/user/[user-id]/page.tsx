055ff58e15b9feda868933a9b50c7bbb799fe282
Eason Ni <easonn999@gmail.com>
2025-06-30 17:54:31 -0400

    fasda

--- src/app/user/[user-id]/page.tsx
+++ src/app/user/[user-id]/page.tsx
@@ -1,8 +1,8 @@
-export default function userPage({ params }: { params: { 'user-id': string } }) {
+export default function userPage({ params }: { params: { userId: string } }) {
   return (
     <div>
       <h1>User Profile</h1>
-      <p>User ID: {params['user-id']}</p>
+      <p>User ID: {params.userId}</p>
       {/* Add user profile content here */}
     </div>
   );
