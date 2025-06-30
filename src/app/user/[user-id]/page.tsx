88822a1b91cc97449f8f87490110a1486a9786f7
Eason Ni <easonn999@gmail.com>
2025-06-30 17:57:15 -0400

    please fix

--- src/app/user/[user-id]/page.tsx
+++ src/app/user/[user-id]/page.tsx
@@ -1,12 +1,12 @@
 interface PageProps {
-  params: { userId: string };
+  params: { 'user-id': string };
 }
 
 export default function UserPage({ params }: PageProps) {
   return (
     <div>
       <h1>User Profile</h1>
-      <p>User ID: {params.userId}</p>
+      <p>User ID: {params['user-id']}</p>
       {/* Add user profile content here */}
     </div>
   );
