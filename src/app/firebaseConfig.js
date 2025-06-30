ad48f3cdec7fb7a41756d3b6280330ff733927ce
AverJung <usorname1@gmail.com>
2025-06-30 13:06:45 -0700

    Create firebaseConfig.js

---
+++ src/app/firebaseConfig.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXyjDL1YFtjho68s5hmcUy4n_putgoXdU",
+  authDomain: "reverse-social-app.firebaseapp.com",
+  projectId: "reverse-social-app",
+}
+
+export default firebaseConfig
