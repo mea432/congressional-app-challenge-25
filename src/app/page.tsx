97b229d29b0830d8b829dafd58c0a0d3ff1df8f7
Eason Ni <easonn999@gmail.com>
2025-06-30 16:45:30 -0400

    improve landing page with links to sign-in and sign-up and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Link href="/sign-in">Sign In</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Landing Page</h1>
+      <Link href="/sign-in" className="mt-4 inline-block text-sm underline-offset-4 hover:underline">
+        Sign In
+      </Link>
       <br />
-      <Link href="/sign-up">Sign Up</Link>
+      <Link href="/sign-up" className="mt-4 inline-block text-sm underline-offset-4 hover:underline">
+        Sign Up
+      </Link>
     </div>
   );
 }
