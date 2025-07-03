9c9ff4e81f30482e0836d754cde7281a6526f5a8
Eason Ni <easonn999@gmail.com>
2025-07-02 22:57:13 -0400

    more fix on mobile view

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MainContent({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center h-[calc(100vh-4rem)] bg-gray-100 pt-16 w-full">
-      <div className="w-full max-w-lg p-4">{children}</div>
+      <div className="w-full max-w-lg p-4 mb-[calc(4rem)]">{children}</div>
     </div>
   );
 }
