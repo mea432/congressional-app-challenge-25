4763e7e9b9bdc8450d7b5040ca662e6bbf1dc8dd
Eason Ni <easonn999@gmail.com>
2025-07-17 14:53:24 -0400

    test commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
+import MainContent from "@/components/main-content"
+
 export default function about() {
   return (
-    <div>
-      <h1>About</h1>
+    <MainContent>
+      <h1 className="text-3xl">About</h1>
       <p>Created by high school students Eason Ni and Avery Chung for the Congressional App Challenge 2025</p>
-    </div>
+    </MainContent>
   )
 }
