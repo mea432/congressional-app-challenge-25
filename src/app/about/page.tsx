f8c33a3c7feec6ca11b7e879f0b143da8d16ccbf
Eason Ni <easonn999@gmail.com>
2025-07-17 14:09:50 -0400

    2026 -> 2025

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function about() {
   return (
     <div>
       <h1>About</h1>
-      <p>Created by high school students Eason Ni and Avery Chung for the Congressional App Challenge 2026</p>
+      <p>Created by high school students Eason Ni and Avery Chung for the Congressional App Challenge 2025</p>
     </div>
   )
 }
